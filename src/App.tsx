bc3621f7188f62be295d8534891e912a5d28bc14
Daniel Gray <gray.daniel@gmail.com>
2022-09-25 08:34:03 -0600

    Scaffolding router, app, etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './style/App.scss';
+import Home from './views/Home';
+import Header from './components/Header/Header';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+			<>
+				<div className="App">
+					<Header />
+					<Routes>
+						<Route path="/" element={<Home />} />
+					</Routes>
+				</div>
+			</>
+	);
 }
 
 export default App;
---
+++ src/views/Home.tsx
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+
+function Home() {
+	return (
+		<>
+			<main>
+				Hello!
+			</main>
+		</>
+	)
+}
+
+export default Home;
