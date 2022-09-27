c2bde8d463384309abb9e1be85ac0af5724572cf
Daniel Gray <gray.daniel@gmail.com>
2022-09-27 17:28:42 -0600

    Technically it add's new fields.

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -1,10 +1,18 @@
 import "../style/Main.scss";
 import AddDeckList from "../components/AddDecklist/AddDecklist";
+import { useState } from "react";
 
-function Home() {
+const Home = () => {
+	const [decklistInputs, setDecklistInputs] = useState([] as any[]);
+
+	const onAddDecklist = () => {
+		setDecklistInputs(decklistInputs.concat(<AddDeckList key={decklistInputs.length} />));
+	}
+
 	return (
 		<main role="main">
-			<AddDeckList />
+			{decklistInputs}
+			<button onClick={onAddDecklist}>Add Deck list</button>
 		</main>
 	)
 }
