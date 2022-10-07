5e3b9d71e23a5942d2ab77de5af3351845c2f8df
Daniel Gray <gray.daniel@gmail.com>
2022-10-06 19:19:29 -0600

    Logos

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import PhyrexianSymbol from "../../images/phyrexian-symbol.png";
 
 const Header = () => (
 	<header role="heading" aria-level={1}>
-		<h1><img src={PhyrexianSymbol} /><a href="/" title="MQR">MQR</a></h1>
+		<h1><img src={PhyrexianSymbol} alt="MQR - Magic QR Generator" /><a href="/" title="MQR">MQR</a></h1>
 		<div>Easily generate printable sheets of QR codes to share your Magic decks in real life!</div>
 	</header>
 )
