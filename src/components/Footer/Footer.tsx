8d71b7dfacb6e24b759a13c68f02177ffe49aa98
Daniel Gray <gray.daniel@gmail.com>
2022-10-06 19:10:50 -0600

    Adding phyrexian logo

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ let currentYear = new Date().getFullYear();
 
 const Footer = () => (
 	<footer role="contentinfo">
-		<span className="left">&copy; {currentYear} <a target="_blank" rel="noreferrer" href="http://twitter.com/behemothdan">BehemothDan</a></span>
+		<span className="left">&copy; {currentYear} <a target="_blank" rel="noreferrer" href="http://twitter.com/behemothdan" title="BehemothDan">BehemothDan</a></span>
 		<span className="right">Wizards of the Coast, Magic: The Gathering, and their logos are trademarks of Wizards of the Coast LLC in the United States and other countries.
 			<br />© 1993-{currentYear} Wizards. All Rights Reserved.
 			<br />
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,9 @@
 import "./Header.scss";
+import PhyrexianSymbol from "../../images/phyrexian-symbol.png";
 
 const Header = () => (
 	<header role="heading" aria-level={1}>
-		<h1>MQR</h1>
+		<h1><img src={PhyrexianSymbol} /><a href="/" title="MQR">MQR</a></h1>
 		<div>Easily generate printable sheets of QR codes to share your Magic decks in real life!</div>
 	</header>
 )
